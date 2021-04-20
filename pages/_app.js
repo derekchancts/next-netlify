3de7ec85ea1e08cd5a27e49d334ce182c947a749
derekchancts <derekchancts@yahoo.com.hk>
2021-04-21 00:28:59 +0800

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import Link from 'next/link'
 import '../styles/globals.css'
 
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+    <Link href="/"><a>Home</a></Link>
+    <Link href="/about"><a>About</a></Link>
+    <Component {...pageProps} />
+    <footer>Copyright chaners 2021</footer>
+    </>
+  )
 }
 
 export default MyApp
